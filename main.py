afea6a88671496515adc4d00f8043d157804c4de
vishal parmar <vpvishalparmar1601@gmail.com>
2024-12-05 14:31:04 +0530

    final update vishal in both file

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'both pipeline is work properlly pipeline buld'
+    return 'hello vishal'
 
 if __name__ == '__main__':
     # Ensures the app is accessible outside the container
